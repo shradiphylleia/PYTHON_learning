601d0b2b46b65b286871c52f28a707dc3715e6bf
Shraddha <118170804+shradiphylleia@users.noreply.github.com>
2023-04-02 17:00:50 +0530

    Add files via upload

---
+++ patterns/numeric/mutiplication pattern.py
@@ -0,0 +1,15 @@
+#mutliplication table pattern
+##1  
+##2  4  
+##3  6  9  
+##4  8  12  16  
+##5  10  15  20  25  
+##6  12  18  24  30  36  
+##7  14  21  28  35  42  49  
+##8  16  24  32  40  48  56  64 
+n=1
+for i in range(1,9):
+    for j in range(1,i+1):
+        print(n*j,end="  ")
+    n=n+1
+    print("")
