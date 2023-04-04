1de9319b7e02e713e858d345feac9e86ff24fa1b
Shraddha <118170804+shradiphylleia@users.noreply.github.com>
2023-04-04 19:26:17 +0530

    Add files via upload

---
+++ course work/as1.py
@@ -0,0 +1,56 @@
+#2
+print("Hello Everyone !!!")
+print("Hello\nWorld")
+print("Hello\n    World")
+print("'Rohit's date of birth is 12\\05\\1999'")
+#3
+#string variable declaration
+x=""
+#assignment
+x="Hello"
+#printing the value of x
+print(x)
+#4
+#integer data type
+a=123
+#floating type data
+b=123.45
+#string type
+c="name"
+#complex type
+i=6+2j
+#boolean type
+d=True
+#printing
+print("integer type value:",a)
+print("floating type value:",b)
+print("string type value:",c)
+print("complex type value:",i)
+print("boolean type value:",d)
+#5
+a="Shraddha"
+b="Sharma"
+full_name=a+b
+print("My name is:",full_name)
+#6
+a="Shraddha"
+b="Sharma"
+c="aru"
+print(a,'(',c,')',b)
+#7
+name="NIKUNJ BANSAL"
+sap_id="500069944"
+dob="13 Oct 1999"
+uni="UPES"
+campus="Bidholi Campus"
+pincode="248007"
+programme="AI & ML"                                                                                      
+semester="2"
+print("NAME:",name)
+print("SAP ID:",sap_id)
+print("DATE OF BIRTH:",dob)
+print("ADDRESS:",uni,"\n\t",campus,"\n\t PINCODE:",pincode)
+print("PROGRAMME:",programme)
+print("SEMESTER:",semester)
+
+
